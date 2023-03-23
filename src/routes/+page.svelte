7ad7aff2387ff478fb009e3f9f82eebeda56f6e5
Jonas_Jones <91549607+J-onasJones@users.noreply.github.com>
2023-03-24 00:47:40 +0100

    initial structure
    svelte kit structure with homepage

---
+++ src/routes/+page.svelte
@@ -0,0 +1,45 @@
+
+<div class="container">
+    <div class="ascii-art">
+    <pre>
+    <code>
+ ___________________
+|                   |
+|   ____     ____   |
+|  |    |   |    |  |
+|  |____|   |____|  |
+|            __     |
+|           |__|    |
+|                   |
+|___________________|
+    </code>
+    </pre>
+
+
+    </div>
+    <div class="links">
+    <a href="home/">Home</a>
+    <a href="the-based/">For The Based(TM)</a>
+    </div>
+    <hr>
+    <div class="social-media">
+    <div class="row">
+        <a href="https://github.com"><i class="fab fa-github"></i></a>
+        <a href="https://www.youtube.com"><i class="fab fa-youtube"></i></a>
+        <a href="https://discord.com"><i class="fab fa-discord"></i></a>
+    </div>
+    <div class="row">
+        <a href="https://www.reddit.com"><i class="fab fa-reddit"></i></a>
+        <a href="https://twitter.com"><i class="fab fa-twitter"></i></a>
+        <a href="https://www.instagram.com"><i class="fab fa-instagram"></i></a>
+    </div>
+    </div>
+</div>
+  
+  <script>
+    // Add script here if needed
+  </script>
+  
+  <style>
+    @import '+page.css';
+  </style>
