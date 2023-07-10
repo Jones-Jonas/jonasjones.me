7c2152b413eaa2d461515783680023394a8bd57d
Jonas_Jones <91549607+J-onasJones@users.noreply.github.com>
2023-07-10 02:56:43 +0200

    added about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    import Footer from "../../components/Footer.svelte";
+import NavBar from "../../components/NavBar.svelte";
+import ParallaxBg from "../../components/ParallaxBg.svelte";
+</script>
+<ParallaxBg>
+    <title>About</title>
+    <div class="container">
+        <!-- please god dont send me to hell for doing this -->
+        <h1> </h1>
+        <h1> </h1>
+        <NavBar />
+        <h1>About</h1>
+        <p><br>
+        
+        
+        </p>
+    </div>
+    <Footer />
+</ParallaxBg>
