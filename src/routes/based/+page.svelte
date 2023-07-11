f1741d9051a64c81b5e3aa75c70b581c486bb11e
Jonas_Jones <91549607+J-onasJones@users.noreply.github.com>
2023-07-11 17:31:13 +0200

    added coming soonish to based and status

--- src/routes/based/+page.svelte
+++ src/routes/based/+page.svelte
@@ -2,11 +2,14 @@
     import NavBar from "../../components/NavBar.svelte";
     import Footer from "../../components/Footer.svelte";
     import ParallaxBg from "../../components/ParallaxBg.svelte";
+    import Padding from "../../components/padding.svelte";
 </script>
 <ParallaxBg>
     <NavBar />
     <title>For the Based</title>
     <div class="container">
+        <Padding />
+        <h1>Coming Soonish</h1>
         <div class="terminal_user">
                 <span class="terminal_user__name__text">based</span>
                 <span class="terminal_user__at__text">@</span>
@@ -25,4 +28,7 @@
 
 <style>
     @import '+page.css';
+    .terminal_user {
+        display: none;
+    }
 </style>
---
+++ src/routes/status/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Footer from "../../components/Footer.svelte";
+import NavBar from "../../components/NavBar.svelte";
+import ParallaxBg from "../../components/ParallaxBg.svelte";
+import Padding from "../../components/padding.svelte";
+</script>
+<ParallaxBg>
+    <title>Status</title>
+    <div class="container">
+        <Padding />
+        <NavBar />
+        <h1>Coming Soonish</h1>
+    </div>
+    <Footer />
+</ParallaxBg>
