77542815ab6a40aaf4a050625d371cce9235b7bf
Jonas_Jones <91549607+J-onasJones@users.noreply.github.com>
2023-07-25 01:53:59 +0200

    maybe this?

--- src/routes/based/+page.svelte
+++ src/routes/based/+page.svelte
@@ -11,22 +11,6 @@
     <div class="container">
         <Padding />
         <h1>Coming Soonish</h1>
-        <div class="terminal_user">
-                <span class="terminal_user__name__text">based</span>
-                <span class="terminal_user__at__text">@</span>
-                <span class="terminal_user__host__text">jonasjones.dev</span>
-                <span class="terminal_user__colon__text">:</span>
-                <span class="terminal_user__path__text">/home/jonasjones</span>
-                <span class="terminal_user__dollar__text">$</span>
-            <input title="terminal-input" type="text" id="terminal-input" autocomplete="off" spellcheck="false" autocapitalize="none" class="sc-ilhmMj iNZnsg" value="">
-        </div>
     </div>
     <Footer />
 </ParallaxBg>
-
-<style>
-    @import '+page.css';
-    .terminal_user {
-        display: none;
-    }
-</style>
