d366ff85bbc9ce4aadc0ffde236f469106a7f179
Jonas_Jones <91549607+J-onasJones@users.noreply.github.com>
2023-08-31 19:24:22 +0200

    added 405 page easteregg

---
+++ src/routes/404/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import NavBar from "../../components/NavBar.svelte";
+    import Footer from "../../components/Footer.svelte";
+    import ParallaxBg from "../../components/ParallaxBg.svelte";
+    import Padding from "../../components/padding.svelte";
+</script>
+
+<ParallaxBg>
+    <NavBar />
+    <title>404</title>
+    <div class="container">
+        <Padding />
+        <h1>405</h1>
+        <p>This is not the 404 page.</p>
+        <p>This is the 405 page.</p>
+        <p>how dare you visit this page -_-</p>
+    </div>
+    <Footer />
+</ParallaxBg>
