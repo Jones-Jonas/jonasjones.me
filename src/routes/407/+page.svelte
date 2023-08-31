9f58115edd4d9e6c4354e490cc2047b8a99ba76c
Jonas_Jones <91549607+J-onasJones@users.noreply.github.com>
2023-08-31 19:58:33 +0200

    added 407

---
+++ src/routes/407/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import NavBar from "../../components/NavBar.svelte";
+    import Footer from "../../components/Footer.svelte";
+    import ParallaxBg from "../../components/ParallaxBg.svelte";
+    import Padding from "../../components/padding.svelte";
+</script>
+
+<ParallaxBg>
+    <NavBar />
+    <title>407</title>
+    <div class="container">
+        <Padding />
+        <h1>why????</h1>
+    </div>
+    <Footer />
+</ParallaxBg>
