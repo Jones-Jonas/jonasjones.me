16b30c245428e0d5019a41766dba75bdfbcadbcd
Jonas_Jones <91549607+J-onasJones@users.noreply.github.com>
2023-08-31 20:07:24 +0200

    added 406

---
+++ src/routes/406/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import NavBar from "../../components/NavBar.svelte";
+    import Footer from "../../components/Footer.svelte";
+    import ParallaxBg from "../../components/ParallaxBg.svelte";
+    import Padding from "../../components/padding.svelte";
+</script>
+
+<ParallaxBg>
+    <NavBar />
+    <title>406</title>
+    <div class="container">
+        <Padding />
+        <h1>This is not acceptable. You must be a robot</h1>
+        <h1>Please visit <a href="https://jonasjones.dev/I-am-a-robot">https://jonasjones.dev/I-am-a-robot</a></h1>
+    </div>
+    <Footer />
+</ParallaxBg>
