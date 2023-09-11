e674294b1bbea1557ac9a87cb7b201364647ac53
Jonas_Jones <me@jonasjones.dev>
2023-09-12 01:42:41 +0200

    added alpha page

---
+++ src/routes/alpha/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import NavBar from "../../components/NavBar.svelte";
+    import Footer from "../../components/Footer.svelte";
+    import ParallaxBg from "../../components/ParallaxBg.svelte";
+    import Padding from "../../components/padding.svelte";
+</script>
+
+<ParallaxBg>
+    <NavBar />
+    <title>Alpha</title>
+    <div class="container">
+        <Padding />
+        <h1>There currently is no Alpha.</h1>
+        <p>Can't believe I actually managed to release this website in under a year.</p>
+    </div>
+    <Footer />
+</ParallaxBg>
