5e37576d0ee410b7a6d40c81051b967126de9603
Jonas_Jones <me@jonasjones.dev>
2023-09-12 02:21:34 +0200

    added home link to alpha

--- src/routes/alpha/+page.svelte
+++ src/routes/alpha/+page.svelte
@@ -12,6 +12,7 @@
         <Padding />
         <h1>There currently is no Alpha.</h1>
         <p>Can't believe I actually managed to release this website in under a year.</p>
+        <p>Go to <a href="/">Home</a></p>
     </div>
     <Footer />
 </ParallaxBg>
